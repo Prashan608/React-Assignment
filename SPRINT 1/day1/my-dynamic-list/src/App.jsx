6111eb260d3eff25bfec97fed66b8173d89708fa
Prashan608 <prashantmish932@gmail.com>
2025-11-12 17:32:29 +0530

    My dynaic list assignment file add Successful

---
+++ SPRINT 1/day1/my-dynamic-list/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const arr = ["React","javaScript","CSS"]
+
+  return (
+    <div>
+      {arr.map(ele => ele)};
+    </div>
+  )
+}
+
+export default App
