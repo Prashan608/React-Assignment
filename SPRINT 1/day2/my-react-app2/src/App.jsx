4748d84ce2788d6cda2ba7a547175194bc45c3c2
Prashan608 <prashantmish932@gmail.com>
2025-11-13 17:27:53 +0530

    File Added Successful

---
+++ SPRINT 1/day2/my-react-app2/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Card title="Profile Card">
+        <button>click me</button>
+        </Card>
+      <Card title="Contact Card">
+        <p>Contact me on mail</p>
+        </Card>
+      <Card title = "Project Card">
+        <p>Your project is make to do list</p>
+      </Card>
+    </>
+  )
+}
+
+export default App
---
+++ SPRINT 1/day2/my-react-app2/src/components/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card({title,children}){
+   
+    return(
+        <div>
+            <h1>{title}</h1>
+            {children}
+        </div>
+    )
+}
+
+export default Card;
