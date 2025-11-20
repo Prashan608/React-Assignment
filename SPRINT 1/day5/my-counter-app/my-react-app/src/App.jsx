f75cbe97c261199de41b73d646c6b9b8a96861c4
Prashan608 <prashantmish932@gmail.com>
2025-11-20 13:38:44 +0530

    React Assignment file Added Successful

---
+++ SPRINT 1/day5/my-counter-app/my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+
+
+  return (
+    <>
+     <Counter value = {5}/>
+    </>
+  )
+}
+
+export default App
---
+++ SPRINT 1/day5/my-counter-app/my-react-app/src/components/Counter.jsx
@@ -0,0 +1,34 @@
+import "../App.css"
+import { useState } from "react";
+
+function Counter ({value}) {
+ 
+    const[count,setCount] = useState(value);
+    
+    function increment () {
+        setCount(count+1);
+    }
+
+    function decrement () {
+
+        if(count>0) setCount(count-1);
+    }
+
+    function reset () {
+
+        setCount(0);
+    }
+
+    return (
+
+        <div>
+            <h1>Count:{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    )
+
+}
+
+export default Counter;
