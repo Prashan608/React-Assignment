a989db12f6f76db45ae84990c42f844b7b4f440a
Prashan608 <prashantmish932@gmail.com>
2025-11-30 20:30:05 +0530

    Assignment file added Successful

---
+++ SPRINT 3/day1/my-react-app/src/redux/action.js
@@ -0,0 +1,15 @@
+
+const increment = "increment";
+const decrement = "decrement";
+
+export const incrementAction = () => (
+    {
+        type : increment
+    }
+);
+
+export const decrementAction = () => (
+    {
+        type : decrement
+    }
+)
